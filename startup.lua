8488e3a4a40b5f3437a631661aa988b8e6ef8200
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-29 17:52:13 -0500

    Monitor redirection

--- startup.lua
+++ startup.lua
@@ -1,4 +1,13 @@
 require("bios")
 
+print("Please specify monitor name or press enter")
+local monitor = io.read()
+
+if monitor and monitor ~= "" and peripheral.isPresent(monitor) then
+    print("Redirecting to monitor " .. monitor)
+    monitor = peripheral.wrap(monitor)
+    prevTerm = term.redirect(monitor)
+end
+
 require("/bin/sh")()
 os.shutdown()
