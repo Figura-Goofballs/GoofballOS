eace41d14bbbe96feb23ce86a78ae39aebc932dd
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-30 17:06:19 -0500

    Add password system, disabling termination while password is being inputted

--- startup.lua
+++ startup.lua
@@ -1,5 +1,35 @@
 require("bios")
 
+local pEvent = os.pullEvent
+os.pullEvent = os.pullEventRaw
+
+::passwordInput::
+if fs.exists("/.password") then
+    write("password: ")
+    local pass = read()
+    local file = fs.open("/.password", "r")
+
+    if (not (pass == file.readAll())) then
+        print("Incorrect password, shutting down")
+        file.close()
+        sleep(1)
+        os.shutdown()
+    end
+    file.close()
+else
+    print("No password exists, what do you want your password to be?")
+
+    local pass = read()
+
+    local file = fs.open("/.password", "w")
+    file.write(pass)
+    file.close()
+
+    goto passwordInput
+end
+
+os.pullEvent = pEvent
+
 print("Please specify monitor name or press enter")
 local monitor = io.read()
 
