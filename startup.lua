1c4803eaa85beff56ecccf280929d262072156fe
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-30 16:37:35 -0500

    BSOD

--- startup.lua
+++ startup.lua
@@ -9,5 +9,40 @@ if monitor and monitor ~= "" and peripheral.isPresent(monitor) then
     prevTerm = term.redirect(monitor)
 end
 
-require("/bin/sh")()
+xpcall(function ()
+    require("/bin/sh")()
+end, function (err)
+    local width, height = term.getSize()
+
+    paintutils.drawFilledBox(1, 1, width, height, colors.blue)
+
+    paintutils.drawPixel(2, 2, colors.white)
+    paintutils.drawPixel(2, 5, colors.white)
+    paintutils.drawLine(4, 3, 4, 4, colors.white)
+    paintutils.drawPixel(5, 2, colors.white)
+    paintutils.drawPixel(5, 5, colors.white)
+
+    term.setBackgroundColor(colors.blue)
+    -- paintUtils.drawLine(7, 3, 27+7, 3, colors.black)
+    term.setCursorPos(8, 3)
+    term.write("An error has occured, error details")
+    -- paintUtils.drawLine(7, 4, 7+18, 4, colors.black)
+    term.setCursorPos(8, 4)
+    term.write("are printed below")
+
+    term.setCursorPos(1, 8)
+    write(err)
+
+    term.setCursorPos(2, height - 1)
+    write("Press any key to restart")
+
+    while true do
+        local event = os.pullEvent()
+
+        if event == "key" then
+            os.reboot()
+        end
+    end
+end)
+
 os.shutdown()
