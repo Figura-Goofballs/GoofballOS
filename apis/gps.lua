bba6576c24fca8ae5a3c3f40e6ef7caf19e529eb
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-31 16:18:10 -0500

    don't use same line

--- apis/gps.lua
+++ apis/gps.lua
@@ -234,12 +234,6 @@ function funcs.host(x, y, z)
             if side == modemSide and channel == gps.CHANNEL_GPS and message == "PING" and distance then
                 modem.transmit(replyChannel, funcs.CHANNEL_GPS, {x, y, z})
 
-                numServed = numServed + 1
-                if numServed > 1 then
-                    local _, y = term.getCursorPos()
-                    term.setCursorPos(1, y - 1)
-                end
-
                 print("Serving request. " .. numServed .. " GPS request" .. (numServed > 1 and "s" or "") .. " served")
             end
         end
