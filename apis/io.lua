d4e4893ddb132b2150dee6c37a11fd2797ab33e2
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-06-03 00:38:26 -0400

    4 spaces

--- apis/io.lua
+++ apis/io.lua
@@ -1,55 +1,55 @@
--- vim: ts=4 sts=4 sw=4 noet ft=lua fdm=marker
+-- vim: ts=4 sts=4 sw=4 et ft=lua fdm=marker
 --{{{ boilerplate/utils
 local native_io = io
 local io = _ENV -- seems shady but it's been working
 local i, o = native_io.stdin, native_io.stdout
 local function is_none(...)
-	return select("#", ...) == 0
+    return select("#", ...) == 0
 end
 --}}}
 --{{{ opening files
 function io.open(f, m)
-	local f, e, c = native_io.open(fs.combine(shell:dir(), f), m)
-	if f then
-		local close = f.close
-		function f:close()
-			if i == f then
-				o = nil
-			end
-			if o == f then
-				o = nil
-			end
-			close(f)
-		end
-		return f
-	else
-		return nil, e, c
-	end
+    local f, e, c = native_io.open(fs.combine(shell:dir(), f), m)
+    if f then
+        local close = f.close
+        function f:close()
+            if i == f then
+                o = nil
+            end
+            if o == f then
+                o = nil
+            end
+            close(f)
+        end
+        return f
+    else
+        return nil, e, c
+    end
 end
 --}}}
 --{{{ implicit files
 function io.input(...)
-	if is_none(...) then
-		return i
-	elseif not ... or (...).read then
-		i = ...
-	else
-		i = io.open(..., "r")
-	end
+    if is_none(...) then
+        return i
+    elseif not ... or (...).read then
+        i = ...
+    else
+        i = io.open(..., "r")
+    end
 end
 function io.output(...)
-	if is_none(...) then
-		return o
-	elseif not ... or (...).write then
-		o = ...
-	else
-		o = io.open(..., "w")
-	end
+    if is_none(...) then
+        return o
+    elseif not ... or (...).write then
+        o = ...
+    else
+        o = io.open(..., "w")
+    end
 end
 --}}}
 --{{{ copy shit
 for _, k in ipairs { "stdin", "stdout", "stderr" } do
-	io[k] = native_io[k]
+    io[k] = native_io[k]
 end
 --}}}
 
